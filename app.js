3ce43c276b384d32c139c61cac6d40f6e3347754
marcosrogeriosilva81-cloud <marcosrogerio.silva81@gmail.com>
2026-05-06 21:55:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const verifyToken = process.env.VERIFY_TOKEN;
 
 // URL do webhook do n8n
-const N8N_WEBHOOK_URL = "https://augmented-elongated-yard.ngrok-free.dev/webhook-test/82e278a1-e3dc-4bf6-b215-6bb760ec5f52/whatsapp-retorno";
+const N8N_WEBHOOK_URL = "https://augmented-elongated-yard.ngrok-free.dev/webhook/82e278a1-e3dc-4bf6-b215-6bb760ec5f52/whatsapp-retorno";
 
 // Verificação Meta
 app.get('/webhook', (req, res) => {
